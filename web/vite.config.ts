811a54b22eaaa6c17e4fcb8e8702c301d85eea0a
Fernando Jose Hernandez Leonett <FH266188@fi365.ort.edu.uy>
2025-08-31 22:25:08 +0200

    docker working

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     port: 8080,
   },
   define: {
-    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL || "http://localhost:5166/api"),
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL || "http://localhost:5167/api"),
   },
 });
