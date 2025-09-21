f496848950995970ff4c136bda02c745971383c2
Fernando Jose Hernandez Leonett <FH266188@fi365.ort.edu.uy>
2025-09-21 17:44:01 +0200

    Refactor Dockerfiles y variables de entorno

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_PORT?: string; // opcional, porque puede no estar definido
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,11 +2,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
+export default ({ mode }: { mode: string }) => {
+  // Cargar solo variables que empiezan con VITE_
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  // Convertir puerto a número
+  const port: number = env.VITE_PORT ? Number(env.VITE_PORT) : 5173;
+
+  return defineConfig({
     plugins: [react()],
-    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-    server: { host: "0.0.0.0", port: Number(env.VITE_PORT)  },
-  };
-});
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "./src") },
+    },
+    server: {
+      host: "0.0.0.0",
+      port,
+    },
+  });
+};
