acb309d65e7e7d7212707a02d9e268d13767ed1b
Fernando Jose Hernandez Leonett <FH266188@fi365.ort.edu.uy>
2025-09-21 16:56:15 +0200

    refacored envoirment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+    server: { host: "0.0.0.0", port: Number(env.VITE_PORT)  },
+  };
 });
